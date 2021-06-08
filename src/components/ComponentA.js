83d35993ac4207b14895c7434986ccac1ecf5ec8
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-08 17:18:17 -0500

    Show a render counter.

--- src/components/ComponentA.js
+++ src/components/ComponentA.js
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
 
+let renderCount = 0;
 const ComponentA = () => {
+  useEffect(() => {
+    renderCount++;
+  })
   return (
     <div>
-
+      <h1>
+        Component A render count: { renderCount }
+      </h1>
     </div>
   )
 }

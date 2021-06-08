62fb0ba814bfd350a13522bb41ff69a4ac80d49c
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-08 17:38:12 -0500

    Import ComponentB Create state for component b Create button to incrementB

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { useState, useMemo } from 'react';
 import ComponentA from 'components/ComponentA';
+import ComponentB from 'components/ComponentB';
 import './App.css';
 
 function App() {
   const [counterA, setCounterA] = useState(0);
+  const [counterB, setCounterB] = useState(0);
   // useMemo only re render if the dependencies have changed.
   const memoComponentA = useMemo(() => <ComponentA count={ counterA } />, [ counterA ]);
 
   const incrementA = () => {
     setCounterA(counterA + 1);
+  }
+  const incrementB = () => {
+    setCounterB(counterA + 1);
   }
 
   return (
@@ -17,7 +22,11 @@ function App() {
       <p>
         <button onClick={ incrementA }>Increment A</button>
       </p>
+      <p>
+        <button onClick={ incrementB }>Increment B</button>
+      </p>
       { memoComponentA }
+      <ComponentB count={ counterB } />
     </div>
   );
 }

b91e1b9179d5903cc929fd06d2660f8fd84b144e
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-08 17:15:52 -0500

    Set counterA using useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counterA, setCounterA] = useState(0);
+
+  const incrementA = () => {
+    setCounterA(counterA + 1);
+  }
+
   return (
     <div className="App">
-
+      App js counter a: { counterA }
+      <p>
+        <button onClick={ incrementA }>Increment A</button>
+      </p>
     </div>
   );
 }
